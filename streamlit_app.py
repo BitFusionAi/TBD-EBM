5205a7fe6592e9ab50e3b467f3364ae8f500c3f2
BitFusionAi <146052148+BitFusionAi@users.noreply.github.com>
2024-11-25 01:34:16 +0000

    Restore database

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,12 @@ import streamlit as st
 from Metrics.tao_price_metrics import display_tao_metrics
 from sn45_rank import display_sn45_rank
 
+# Set the title and favicon that appear in the Browser's tab bar.
+st.set_page_config(
+    page_title='TBD EBM',
+    page_icon=':pick:',
+)
+
 # Title
 st.title("TBD ⛏️")
 
